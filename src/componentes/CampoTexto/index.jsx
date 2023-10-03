eff03b08f4df75c477877ec0f70a5ff2c5abc2b0
PeuStone <pedrohmotaaa@gmail.com>
2023-10-03 10:10:04 -0300

    Adicionando campo de input ao Header do projeto

---
+++ src/componentes/CampoTexto/index.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+import search from '/imagens/Lupa.png'
+
+const ContainerEstilizado = styled.div`
+    position: relative;
+    display: inline-block;
+`
+
+const CampoTextoEstilizado = styled.input`
+    width: 566px;
+    height: 56px;
+    padding: 12px 16px;
+    border-radius: 10px;
+    border-width: 2px;
+    border: 2px solid #C98CF1;
+    background: transparent;
+    box-sizing: border-box;
+    color: #D9D9D9;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 20px;
+`
+
+const Lupa = styled.img`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 38px;
+    height: 38px;
+`
+
+const CampoTexto = (props) => {
+    return (
+        <ContainerEstilizado>
+            <CampoTextoEstilizado placeholder="O que você procura?" {...props} />
+            <Lupa src={search} alt="icone de lupa" />
+        </ContainerEstilizado>
+    )
+}
+
+export default CampoTexto
