3f9b5863e449e2ecdd26d8629591d0e9d2055cea
PeuStone <pedrohmotaaa@gmail.com>
2023-10-17 09:40:21 -0300

    Editando e personalizando o modal do projeto

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,12 +1,33 @@
+import styled from 'styled-components'
+import Imagem from '../Galeria/imagem'
+
+const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
+
 const ModalZoom = ({ foto }) => {
     return (
         <>
-            <dialog open={!!foto}>
-                <p>Greeting, one and all!</p>
-                <form method="dialog">
-                    <button>OK</button>
-                </form>
-            </dialog>
+            {foto && <>
+                <Overlay />
+                <DialogEstilizado open={!!foto}>
+                    <Imagem foto={foto} expandida={true} />
+                    <form method="dialog">
+                        <button>OK</button>
+                    </form>
+                </DialogEstilizado>
+            </>
+            }
         </>
     )
 }
